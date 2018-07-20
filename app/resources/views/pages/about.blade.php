f62d6d28880579c96d0930ae554c6d61b1819a5e
bruce8 <brucealvero@gmail.com>
2018-07-20 11:41:22 +0800

    used the Blade engine

--- app/resources/views/pages/about.blade.php
+++ app/resources/views/pages/about.blade.php
@@ -1,15 +1,6 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>LARAVEL</title>
-
-    </head>
-    <body>
-        <h1>About</h1>
-        <p>This is the About page</p>
-    </body>
-</html>
+@section('content')
+    <h1>{{ $title }}</h1>
+    <p>This is the About page</p>
+@endsection

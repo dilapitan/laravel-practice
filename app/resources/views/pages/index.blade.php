432bd2d80fa36084860809e9773e9ce7d8819c81
bruce8 <brucealvero@gmail.com>
2018-07-20 11:08:48 +0800

    created different controllers and routes

---
+++ app/resources/views/pages/index.blade.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>LARAVEL</title>
+
+    </head>
+    <body>
+       <h1>Index</h1>
+        <p>This is the Index page</p>
+    </body>
+</html>

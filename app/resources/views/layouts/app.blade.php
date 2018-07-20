662aea7ed63d3492e7aa65b57d6906add3636efc
bruce8 <brucealvero@gmail.com>
2018-07-20 16:42:00 +0800

    added navbar

--- app/resources/views/layouts/app.blade.php
+++ app/resources/views/layouts/app.blade.php
@@ -4,11 +4,13 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>LARAVEL</title>
-
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <title>LARAVEL</title>
     </head>
     <body>
-       @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
